483fd2178df5dfdc8084312c2e7aa0c5b83a27ce
Konuralp <konuralpcelikyay@gmail.com>
2023-02-03 00:35:50 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let generateEl = document.getElementById("generate-el");
+let firstEl = document.getElementById("first-el");
+let secondEl = document.getElementById("second-el");
+let lenEl = document.getElementById("len");
+let len = 0;
+
+
+lenEl.addEventListener('input' ,function(){
+
+len = parseFloat(lenEl.value);
+
+
+});
+
+
+
+
+generateEl.addEventListener("click", function(){
+    
+    let firstPas = "";
+    let secondPas = "";
+    
+    for(let i = 0; i<len; i++){
+        firstPas += characters[Math.floor(Math.random()* characters.length)];
+        secondPas += characters[Math.floor(Math.random()* characters.length)]; 
+    }
+    
+    firstEl.textContent = firstPas;
+    secondEl.textContent = secondPas;
+    
+    
+    
+})
+
+
+
